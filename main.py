74fcc33dde7f56373b8cc8bfd2238eeaf4e4f9f8
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2025-11-20 16:51:41 +0100

    random excel list of 2000 people, id + name + second name

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from openpyxl import Workbook
-import time
+import random
 
 wb = Workbook()
 ws = wb.active
 
-ws['A1'] = "Product"
-ws['B1'] = "Price"
+ws['A1'] = "ID:"
+ws['B1'] = "Name:"
+ws['C1'] = "Second name:"
 
-products_price = {"Milk": 54,
-                  "Bread": 45,
-                  "Water": 47,
-                  "Ice": 12,
-                  }
+ws.column_dimensions["A"].width = 5
+ws.column_dimensions["B"].width = 10
+ws.column_dimensions["C"].width = 12
 
-row = 2
-total = 0
+names = ["Tom", "Bob", "Steve", "King", "Lord", "Voldemort",
+         "Harry", "Roan", "Allison", "Luke", "Paul"]
 
-for product, price in products_price.items():
-    ws.append([product, f"{price} $"])
-    row += 1
-    total += price
-    time.sleep(1)
+second_names = ["Green", "White", "Mclaren", "Black",
+                "Burgers", "Rowling", "Dvorak"]
 
-row += 1
-ws[f'A{row}'] = "Total:"
-ws[f'B{row}'] = f"{total} $"
+id_person = 1
+
+for _ in range(2000):
+    name = random.choice(names)
+    second = random.choice(second_names)
+    ws.append([id_person, name, second])
+    id_person += 1
 
 wb.save("products.xlsx")
